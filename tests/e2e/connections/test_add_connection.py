15f58f8bf30c23676a9f56011a09d0f36de4f9d7
Víctor Jöel Viejo Álvarez <victorjoelviejoalvarez@gmail.com>
2026-08-08 15:32:28 +0200

    [Test] Solucionado error con los tests relacionado a eventos de Qt.

--- tests/e2e/connections/test_add_connection.py
+++ tests/e2e/connections/test_add_connection.py
@@ -119,45 +119,15 @@ def test_create_postgresql_connection_success(
         main_window,
     )
 
-    qtbot.keyClicks(
-        form.name_input,
-        "PostgreSQL connection",
-    )
+    form.name_input.setText("PostgreSQL connection")
+    form.driver_input.setCurrentText(Driver.POSTGRESQL.value)
+    form.host_input.setText("localhost")
+    form.port_input.setText("5432")
+    form.database_input.setText("postgres")
+    form.username_input.setText("postgres")
+    form.password_input.setText("postgres")
 
-    form.driver_input.setCurrentText(
-        Driver.POSTGRESQL.value,
-    )
-
-    qtbot.keyClicks(
-        form.host_input,
-        "localhost",
-    )
-
-    form.port_input.setFocus()
-    qtbot.keyClicks(
-        form.port_input,
-        "5432",
-    )
-
-    qtbot.keyClicks(
-        form.database_input,
-        "postgres",
-    )
-
-    qtbot.keyClicks(
-        form.username_input,
-        "postgres",
-    )
-
-    qtbot.keyClicks(
-        form.password_input,
-        "postgres",
-    )
-
-    qtbot.mouseClick(
-        form.save_button,
-        Qt.MouseButton.LeftButton,
-    )
+    form.save_button.click()
 
     connections = get_all_connections()
 
@@ -206,6 +176,176 @@ def test_create_postgresql_connection_invalid_data(
         form.save_button,
         Qt.MouseButton.LeftButton,
     )
+
+    assert get_all_connections() == []
+
+    assert main_window.stack.currentWidget() is main_window.connection_form_page
+
+    assert main_window.sidebar.connections_list.list_widget.count() == 0
+
+
+def test_create_mysql_connection_success(
+    qtbot: QtBot,
+    main_window: MainWindow,
+):
+    """
+    Verifica que se crea correctamente una conexión MySQL válida.
+    """
+
+    form = _open_connection_form(
+        qtbot,
+        main_window,
+    )
+
+    form.name_input.setText("MySQL connection")
+    form.driver_input.setCurrentText(
+        Driver.MYSQL.value,
+    )
+    form.host_input.setText("localhost")
+    form.port_input.setText("3306")
+    form.database_input.setText("mysql")
+    form.username_input.setText("mysql")
+    form.password_input.setText("mysql")
+
+    form.save_button.click()
+
+    connections = get_all_connections()
+
+    assert len(connections) == 1
+
+    connection = connections[0]
+
+    assert connection.name == "MySQL connection"
+    assert connection.driver == Driver.MYSQL
+    assert connection.host == "localhost"
+    assert connection.port == 3306
+    assert connection.database == "mysql"
+    assert connection.username == "mysql"
+    assert connection.password == "mysql"
+
+    assert main_window.stack.currentWidget() is main_window.home_page
+
+    assert main_window.sidebar.connections_list.list_widget.count() == 1
+
+
+def test_create_mysql_connection_invalid_data(
+    qtbot: QtBot,
+    main_window: MainWindow,
+):
+    """
+    Verifica que no se crea una conexión MySQL con datos inválidos.
+    """
+
+    form = _open_connection_form(
+        qtbot,
+        main_window,
+    )
+
+    form.name_input.setText(
+        "Broken connection",
+    )
+
+    form.driver_input.setCurrentText(
+        Driver.MYSQL.value,
+    )
+
+    # Dejamos todos los campos obligatorios vacíos.
+
+    form.save_button.click()
+
+    assert get_all_connections() == []
+
+    assert main_window.stack.currentWidget() is main_window.connection_form_page
+
+    assert main_window.sidebar.connections_list.list_widget.count() == 0
+
+
+def test_create_oracle_connection_success(
+    qtbot: QtBot,
+    main_window: MainWindow,
+):
+    """
+    Verifica que se crea correctamente una conexión Oracle válida.
+    """
+
+    form = _open_connection_form(
+        qtbot,
+        main_window,
+    )
+
+    form.name_input.setText(
+        "Oracle connection",
+    )
+
+    form.driver_input.setCurrentText(
+        Driver.ORACLE.value,
+    )
+
+    form.host_input.setText(
+        "localhost",
+    )
+
+    form.port_input.setText(
+        "1521",
+    )
+
+    form.database_input.setText(
+        "oracle",
+    )
+
+    form.username_input.setText(
+        "oracle",
+    )
+
+    form.password_input.setText(
+        "oracle",
+    )
+
+    form.save_button.click()
+
+    connections = get_all_connections()
+
+    assert len(connections) == 1
+
+    connection = connections[0]
+
+    assert connection.name == "Oracle connection"
+    assert connection.driver == Driver.ORACLE
+    assert connection.host == "localhost"
+    assert connection.port == 1521
+    assert connection.database == "oracle"
+    assert connection.username == "oracle"
+    assert connection.password == "oracle"
+
+    assert main_window.stack.currentWidget() is main_window.home_page
+
+    assert main_window.sidebar.connections_list.list_widget.count() == 1
+
+
+def test_create_oracle_connection_invalid_data(
+    qtbot: QtBot,
+    main_window: MainWindow,
+):
+    """
+    Verifica que no se crea una conexión Oracle con datos inválidos.
+    """
+
+    form = _open_connection_form(
+        qtbot,
+        main_window,
+    )
+
+    form.name_input.setText(
+        "Broken connection",
+    )
+
+    form.driver_input.setCurrentText(
+        Driver.ORACLE.value,
+    )
+
+    # Dejamos todos los campos obligatorios vacíos.
+
+    form.save_button.click()
 
     assert get_all_connections() == []
 
